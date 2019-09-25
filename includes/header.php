7795626f383058bfc1548834a9960a319489eb4e
Nick Ferrari <njferrari14@gmail.com>
2019-09-24 19:43:12 -0500

    Initial commit. Just starting out.

---
+++ includes/header.php
@@ -0,0 +1,19 @@
+<?php
+// require_once './connection.php';
+// require_once './utility_funcs.php';
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Nicholas Ferrari</title>
+</head>
+<body>
+<nav>
+	<ul>
+		<li><a href="home.php">Home</a></li>
+		<li><a href="about.php">About</a></li>
+		<li><a href="work.php">Work</a></li>
+		<li><a href="contact.php">Contact</a></li>
+	</ul>
+</nav>
