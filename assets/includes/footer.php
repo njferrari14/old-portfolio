a3dee1b26d68e86e5c36e0f163933f7f0aa47e9c
Nick Ferrari <njferrari14@gmail.com>
2020-03-12 08:11:00 -0500

    updated twitter link

--- assets/includes/footer.php
+++ assets/includes/footer.php
@@ -5,7 +5,7 @@
 	<div class="social">
 		
 		<h3 class="h-two">Follow me!</h3>
-		<a href="https://twitter.com/Krozmo_KMer"><img class="icon" src="assets/images/icons/twitter.png" alt="Twitter"></a>
+		<a href="https://twitter.com/nj_ferrari"><img class="icon" src="assets/images/icons/twitter.png" alt="Twitter"></a>
 		<a href="https://www.linkedin.com/in/njferrari14/"><img class="icon" src="assets/images/icons/linkedin.png" alt="LinkedIn"></a>
 		<a href="https://github.com/njferrari14"><img class="icon" src="assets/images/icons/octocat.png" alt="GitHub"></a>
 
